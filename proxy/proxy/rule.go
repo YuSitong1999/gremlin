5e255bdb9593f12a5c7e56038a77ecc590b9e627
于斯同 <yusitong1999@foxmail.com>
2022-07-01 18:18:38 +0800

    simplify NewRule, getDistribution, and add getMessageType

--- proxy/proxy/rule.go
+++ proxy/proxy/rule.go
@@ -75,56 +75,87 @@ var NopRule = Rule{Enabled: false}
 
 // 分布名字 -> 编号
 func getDistribution(distribution string) (ProbabilityDistribution, error) {
-
-	if distribution == "" {
-		return ProbUniform, nil
-	}
-
 	switch str.ToLower(distribution) {
-	case "uniform":
+	case "uniform", "":
 		return ProbUniform, nil
 	case "exponential":
 		return ProbExponential, nil
 	case "normal":
 		return ProbNormal, nil
 	default:
-		return ProbUniform, errors.New("Unknown probability distribution")
+		return ProbUniform, errors.New("unknown probability distribution")
+	}
+}
+
+// 消息类型名字 -> 编号
+func getMessageType(messageType string) (MessageType, error) {
+	switch str.ToLower(messageType) {
+	case "request":
+		return Request, nil
+	case "response":
+		return Response, nil
+	case "publish":
+		return Publish, nil
+	case "subscribe":
+		return Subscribe, nil
+	default:
+		return MTypeUnknown, errors.New("unsupported request type")
 	}
 }
 
 // NewRule return a new rule based on the config.
-func NewRule(c config.RuleConfig) (Rule, error) {
-	var r Rule
-	var err error
-	// Convert request/reply types
-	switch str.ToLower(c.MType) {
-	case "request":
-		r.MType = Request
-	case "response":
-		r.MType = Response
-	case "publish":
-		r.MType = Publish
-	case "subscribe":
-		r.MType = Subscribe
-	default:
-		return NopRule, errors.New("Unsupported request type")
+func NewRule(c config.RuleConfig) (r Rule, err error) {
+	r = Rule{
+		Enabled: true,
+		Source:  c.Source,
+		Dest:    c.Dest,
+		//MType:              0,
+		BodyPattern:      c.BodyPattern,
+		HeaderPattern:    c.HeaderPattern,
+		DelayProbability: c.DelayProbability,
+		//DelayDistribution:  0,
+		DelayTime:         time.Duration(0), // default
+		MangleProbability: c.MangleProbability,
+		//MangleDistribution: 0,
+		SearchString:     c.SearchString,
+		ReplaceString:    c.ReplaceString,
+		AbortProbability: c.AbortProbability,
+		//AbortDistribution:  0,
+		ErrorCode: c.ErrorCode,
 	}
-	r.BodyPattern = c.BodyPattern
-	r.HeaderPattern = c.HeaderPattern
+
 	//sanity check
 	//at least header or body pattern must be non-empty
 	if r.HeaderPattern == "" {
 		return NopRule, errors.New("HeaderPattern cannot be empty (specify * instead)")
 	}
-
 	if r.BodyPattern == "" {
 		r.BodyPattern = "*"
+	}
+
+	// 至少有一种故障
+	valid := r.DelayProbability > 0.0 || r.MangleProbability > 0.0 || r.AbortProbability > 0.0
+	if !valid {
+		return NopRule, errors.New("at least one of delay probability, mangle probability, abort probability must be non-zero and <=1.0")
+	}
+
+	// 设置消息类型
+	if r.MType, err = getMessageType(c.MType); err != nil {
+		return NopRule, err
 	}
 
 	r.DelayDistribution, err = getDistribution(c.DelayDistribution)
 	if err != nil {
 		return NopRule, err
 	}
+	if c.DelayTime != "" {
+		r.DelayTime, err = time.ParseDuration(c.DelayTime)
+		if err != nil {
+			globallog.WithField("errmsg", err.Error()).Warn("Could not parse rule delay time")
+			return NopRule, err
+		}
+	}
+
 	r.MangleDistribution, err = getDistribution(c.MangleDistribution)
 	if err != nil {
 		return NopRule, err
@@ -135,35 +166,7 @@ func NewRule(c config.RuleConfig) (Rule, error) {
 		return NopRule, err
 	}
 
-	r.DelayProbability = c.DelayProbability
-	r.MangleProbability = c.MangleProbability
-	r.AbortProbability = c.AbortProbability
-	// 至少有一种故障
-	valid := ((r.DelayProbability > 0.0) || (r.MangleProbability > 0.0) || (r.AbortProbability > 0.0))
-	if !valid {
-		return NopRule, errors.New("Atleast one of delayprobability, mangleprobability, abortprobability must be non-zero and <=1.0")
-	}
-
-	// 大于1的概率实际上视为1，必然发生
-
-	if c.DelayTime != "" {
-		var err error
-		r.DelayTime, err = time.ParseDuration(c.DelayTime)
-		if err != nil {
-			globallog.WithField("errmsg", err.Error()).Warn("Could not parse rule delay time")
-			return NopRule, err
-		}
-	} else {
-		r.DelayTime = time.Duration(0)
-	}
-
-	r.ErrorCode = c.ErrorCode
-	r.SearchString = c.SearchString
-	r.ReplaceString = c.ReplaceString
-	r.Source = c.Source
-	r.Dest = c.Dest
-	r.Enabled = true
-	return r, nil
+	return
 }
 
 // ToConfig 输出规则的可读版本 converts the rule into a human-readable string config.
